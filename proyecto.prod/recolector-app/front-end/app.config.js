49d1f842a9af5939d35032709326f2e0588201de
GimenezAlfredo <alfredogimenez431@gmail.com>
2025-12-08 10:44:43 -0300

    config-deploy-app-recolector-1

--- proyecto.prod/recolector-app/front-end/app.config.js
+++ proyecto.prod/recolector-app/front-end/app.config.js
@@ -2,11 +2,17 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "Re-colecta-App",
-    slug: "Re-colecta-App",
+    name: "RecolectorApp",
+    slug: "recolectapp-recolector",
+    android: {
+      package: "com.recolectapp.recolector"
+   },
     extra: {
       apiUrl: process.env.API_URL,
       googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+     eas: {
+        projectId: "70328863-4493-46f7-9e44-fd1dba42ab1d"
+      }
     }
   }
 };
